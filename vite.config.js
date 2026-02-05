a5a96aa9d524ee8b25a3a9868b8a12bc00628acb
Krazay17 <Joshmass7@gmail.com>
2026-02-05 17:31:37 -0500

    deployment test 2

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import wasm from "vite-plugin-wasm";
 import topLevelAwait from "vite-plugin-top-level-await";
 
 export default defineConfig({
+  base: "/solblade2/",
   plugins: [wasm(), topLevelAwait()],
   resolve: {
     alias: {
